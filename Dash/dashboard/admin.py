22d138b8b93f8d145eff2d75822004a6b63c1b43
kevin <kevin.aroca18@gmail.com>
2022-05-24 15:52:54 -0400

    panel de administracion montado

--- Dash/dashboard/admin.py
+++ Dash/dashboard/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Usuarios
+from .models import Departamentos
+from .models import Num_telefono
+from .models import Series
+from .models import Historial
+from .models import Equipos
+
+admin.site.register(Usuarios)
+admin.site.register(Departamentos)
+admin.site.register(Num_telefono)
+admin.site.register(Series)
+admin.site.register(Historial)
+admin.site.register(Equipos)
