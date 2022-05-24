b6280b536faa658b9c048706b53e8b0f1ef24868
kevin <kevin.aroca18@gmail.com>
2022-05-24 12:31:56 -0400

    initial commit

---
+++ Dash/dashboard/models.py
@@ -0,0 +1,45 @@
+#from __future__ import unicode_literals
+from enum import auto
+from pickle import TRUE
+from django.db import models
+from django.forms import NullBooleanField
+from django.utils import timezone
+
+class  Usuarios(models.Model):
+    rut = models.CharField(max_length=10, null=False, blank=False, unique=True,primary_key=True)
+    nombre = models.CharField(max_length=20, null=False)
+    apellido = models.CharField(max_length=20, null=False)
+    area = models.ForeignKey(Departamentos,to_field=area) #revisar
+    correo = models.CharField(max_length=50)
+    Telefono = models.ForeignKey(Num_telefono,to_field=numero_tel) #revisar
+
+class Departamentos(models.Model):
+    id = models.IntegerField(null=False,blank=False,unique=True,primary_key=True)
+    area = models.CharField(max_length=20,null=False)
+    sucursal = models.CharField(max_length=20,null=False)
+
+class Num_telefono(models.Model):
+    id = models.IntegerField(null=False,blank=False,unique=True)
+    numero_tel = models.CharField(max_length=10,null=False,blank=False,unique=True)
+    activo = models.BooleanField(default=False) #numero activo o dado de baja
+
+class Equipos(models.Model):
+    id = models.IntegerField(unique=True, null=False,blank=False,primary_key=True)
+    usuario = models.ForeignKey(Usuarios)
+    tipo = models.CharField(max_length=10,null=False)
+    modelo = models.CharField(max_length=20,null=False)
+    serie = models.ForeignKey(Series,to_field=serie)
+    fecha_compra = models.DateTimeField(auto_now_add=true)
+    operativo = models.BooleanField(default=True, null=False)
+    nuevo = models.NullBooleanField(default=True,null=False) #Nuevo / Usado
+    observaciones = models.CharField(max_length=50) #pantalla rota, con mica, etc.
+
+class Series(models.Model):
+    id = models.IntegerField(null=False,blank=False,unique=True,primary_key=True)
+    serie = models.CharField(max_length=20,null=False,blank=False,unique=True)
+    fecha_compra = models.DateTimeField(default=timezone.now) #Establece la fecha local
+    valor = models.IntegerField(blank=True)
+    imei_1 = models.IntegerField(max_length=20,blank=True,null=True)
+    imei_2 = models.IntegerField(max_length=20,blank=True,null=True)
+
+# Create your models here.
