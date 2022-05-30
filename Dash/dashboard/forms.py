21d65b3d49007548a714b0859f50bc8bbb565c31
Marcelo Carvacho <>
2022-05-30 16:08:02 -0400

    forms

--- Dash/dashboard/forms.py
+++ Dash/dashboard/forms.py
@@ -1,4 +1,10 @@
 from django import forms
+from .models import *
 
 class nombre_usuario(forms.Form):
     your_name = forms.CharField(label='nombre_usuario', max_length=20)
+    
+class usuario_form(forms.ModelForm):
+    class Meta:
+        model = Usuarios
+        fields = ['rut', 'nombre','area', 'correo', 'Telefono']
